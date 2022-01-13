2ed6a8c631ca6259b5bf447cdd5d00a367c00828
Seung Hyun Kim <skim449@illinois.edu>
2022-01-13 09:17:59 -0600

    update

--- gym_softrobot/envs/snake/continuum_snake.py
+++ gym_softrobot/envs/snake/continuum_snake.py
@@ -1,7 +1,6 @@
 __doc__ = """Snake friction case from X. Zhang et. al. Nat. Comm. 2021"""
 
-""" This environment is constructed based on the PyElastica-continuum snake """
-
+""" This environment is constructed based on the PyElastica:ContinuumSnakeCase """
 
 import gym
 from gym import error, spaces, utils
@@ -9,164 +8,55 @@ from gym.utils import seeding
 
 from elastica import *
 
-from examples.ContinuumSnakeCase.continuum_snake_postprocessing import (
-    plot_snake_velocity,
-    plot_video,
-    compute_projected_velocity,
-    plot_curvature,
-)
+import numpy as np
 
-class ContinuumSnakeEnv(gym.Env):
-    metadata = {'render.modes': ['rgb', 'human']}
+#from examples.ContinuumSnakeCase.continuum_snake_postprocessing import (
+#    plot_snake_velocity,
+#    plot_video,
+#    compute_projected_velocity,
+#    plot_curvature,
+#)
 
-    def __init__(self):
-        pass
-
-    def step(self, action):
-        pass
-
-    def reset(self):
-        pass
-
-    def render(self, mode='human', close=False):
-        pass
-
-    def close(self):
-        pass
 
 class SnakeSimulator(BaseSystemCollection, Constraints, Forcing, CallBacks):
     pass
 
 
-def run_snake(
-    b_coeff, PLOT_FIGURE=False, SAVE_FIGURE=False, SAVE_VIDEO=False, SAVE_RESULTS=False
-):
-    # Initialize the simulation class
-    snake_sim = SnakeSimulator()
+class ContinuumSnakeEnv(gym.Env):
+    metadata = {'render.modes': ['rgb', 'human']}
 
-    # Simulation parameters
-    period = 2
-    final_time = (11.0 + 0.01) * period
-    time_step = 8e-6
-    total_steps = int(final_time / time_step)
-    rendering_fps = 60
-    step_skip = int(1.0 / (rendering_fps * time_step))
+    def __init__(self):
+        wave_length = 1.0
+        t_coeff_optimized = np.array(
+            [3.4e-3, 3.3e-3, 4.2e-3, 2.6e-3, 3.6e-3, 3.5e-3]
+        )
+        t_coeff_optimized = np.hstack((t_coeff_optimized, wave_length))
 
-    # setting up test params
-    n_elem = 50
-    start = np.zeros((3,))
-    direction = np.array([0.0, 0.0, 1.0])
-    normal = np.array([0.0, 1.0, 0.0])
-    base_length = 0.35
-    base_radius = base_length * 0.011
-    density = 1000
-    nu = 1e-4
-    E = 1e6
-    poisson_ratio = 0.5
-    shear_modulus = E / (poisson_ratio + 1.0)
+        # Determinism
+        self.seed()
 
-    shearable_rod = CosseratRod.straight_rod(
-        n_elem,
-        start,
-        direction,
-        normal,
-        base_length,
-        base_radius,
-        density,
-        nu,
-        E,
-        shear_modulus=shear_modulus,
-    )
+    def seed(self, seed=None):
+        # Deprecated in new gym
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
-    snake_sim.append(shearable_rod)
+    def reset(self):
+        self._build()
+        pass
 
-    # Add gravitational forces
-    gravitational_acc = -9.80665
-    snake_sim.add_forcing_to(shearable_rod).using(
-        GravityForces, acc_gravity=np.array([0.0, gravitational_acc, 0.0])
-    )
+    def step(self, action):
+        err_msg = f"{action!r} ({type(action)}) invalid: expected {self.action_space}"
+        assert self.action_space.contains(action), err_msg
 
-    # Add muscle torques
-    wave_length = b_coeff[-1]
-    snake_sim.add_forcing_to(shearable_rod).using(
-        MuscleTorques,
-        base_length=base_length,
-        b_coeff=b_coeff[:-1],
-        period=period,
-        wave_number=2.0 * np.pi / (wave_length),
-        phase_shift=0.0,
-        rest_lengths=shearable_rod.rest_lengths,
-        ramp_up_time=period,
-        direction=normal,
-        with_spline=True,
-    )
+        b_coeff = action
 
-    # Add friction forces
-    origin_plane = np.array([0.0, -base_radius, 0.0])
-    normal_plane = normal
-    slip_velocity_tol = 1e-8
-    froude = 0.1
-    mu = base_length / (period * period * np.abs(gravitational_acc) * froude)
-    kinetic_mu_array = np.array(
-        [mu, 1.5 * mu, 2.0 * mu]
-    )  # [forward, backward, sideways]
-    static_mu_array = np.zeros(kinetic_mu_array.shape)
-    snake_sim.add_forcing_to(shearable_rod).using(
-        AnisotropicFrictionalPlane,
-        k=1.0,
-        nu=1e-6,
-        plane_origin=origin_plane,
-        plane_normal=normal_plane,
-        slip_velocity_tol=slip_velocity_tol,
-        static_mu_array=static_mu_array,
-        kinetic_mu_array=kinetic_mu_array,
-    )
+        # Compute the average forward velocity. These will be used for optimization.
+        [_, _, avg_forward, avg_lateral] = compute_projected_velocity(pp_list, period)
 
-    # Add call backs
-    class ContinuumSnakeCallBack(CallBackBaseClass):
-        """
-        Call back function for continuum snake
-        """
+        return avg_forward, avg_lateral, pp_list
 
-        def __init__(self, step_skip: int, callback_params: dict):
-            CallBackBaseClass.__init__(self)
-            self.every = step_skip
-            self.callback_params = callback_params
 
-        def make_callback(self, system, time, current_step: int):
-
-            if current_step % self.every == 0:
-
-                self.callback_params["time"].append(time)
-                self.callback_params["step"].append(current_step)
-                self.callback_params["position"].append(
-                    system.position_collection.copy()
-                )
-                self.callback_params["velocity"].append(
-                    system.velocity_collection.copy()
-                )
-                self.callback_params["avg_velocity"].append(
-                    system.compute_velocity_center_of_mass()
-                )
-
-                self.callback_params["center_of_mass"].append(
-                    system.compute_position_center_of_mass()
-                )
-                self.callback_params["curvature"].append(system.kappa.copy())
-
-                return
-
-    pp_list = defaultdict(list)
-    snake_sim.collect_diagnostics(shearable_rod).using(
-        ContinuumSnakeCallBack, step_skip=step_skip, callback_params=pp_list
-    )
-
-    snake_sim.finalize()
-
-    timestepper = PositionVerlet()
-    integrate(timestepper, snake_sim, final_time, total_steps)
-
-    if PLOT_FIGURE:
+    def render(self, mode='human', close=False):
         filename_plot = "continuum_snake_velocity.png"
         plot_snake_velocity(pp_list, period, filename_plot, SAVE_FIGURE)
         plot_curvature(pp_list, shearable_rod.rest_lengths, period, SAVE_FIGURE)
@@ -181,72 +71,131 @@ def run_snake(
                 ylim=(-1, 1),
             )
 
-    if SAVE_RESULTS:
-        import pickle
+    def close(self):
+        pass
 
-        filename = "continuum_snake.dat"
-        file = open(filename, "wb")
-        pickle.dump(pp_list, file)
-        file.close()
+    def _build(self):
+        # Initialize the simulation class
+        snake_sim = SnakeSimulator()
 
-    # Compute the average forward velocity. These will be used for optimization.
-    [_, _, avg_forward, avg_lateral] = compute_projected_velocity(pp_list, period)
+        # Simulation parameters
+        period = 2
+        final_time = (11.0 + 0.01) * period
+        time_step = 8e-6
+        total_steps = int(final_time / time_step)
+        rendering_fps = 60
+        step_skip = int(1.0 / (rendering_fps * time_step))
 
-    return avg_forward, avg_lateral, pp_list
+        # setting up test params
+        n_elem = 50
+        start = np.zeros((3,))
+        direction = np.array([0.0, 0.0, 1.0])
+        normal = np.array([0.0, 1.0, 0.0])
+        base_length = 0.35
+        base_radius = base_length * 0.011
+        density = 1000
+        nu = 1e-4
+        E = 1e6
+        poisson_ratio = 0.5
+        shear_modulus = E / (poisson_ratio + 1.0)
 
-
-if __name__ == "__main__":
-
-    # Options
-    PLOT_FIGURE = True
-    SAVE_FIGURE = True
-    SAVE_VIDEO = True
-    SAVE_RESULTS = False
-    CMA_OPTION = False
-
-    if CMA_OPTION:
-        import cma
-
-        SAVE_OPTIMIZED_COEFFICIENTS = False
-
-        def optimize_snake(spline_coefficient):
-            [avg_forward, _, _] = run_snake(
-                spline_coefficient,
-                PLOT_FIGURE=False,
-                SAVE_FIGURE=False,
-                SAVE_VIDEO=False,
-                SAVE_RESULTS=False,
-            )
-            return -avg_forward
-
-        # Optimize snake for forward velocity. In cma.fmin first input is function
-        # to be optimized, second input is initial guess for coefficients you are optimizing
-        # for and third input is standard deviation you initially set.
-        optimized_spline_coefficients = cma.fmin(optimize_snake, 7 * [0], 0.5)
-
-        # Save the optimized coefficients to a file
-        filename_data = "optimized_coefficients.txt"
-        if SAVE_OPTIMIZED_COEFFICIENTS:
-            assert filename_data != "", "provide a file name for coefficients"
-            np.savetxt(filename_data, optimized_spline_coefficients, delimiter=",")
-
-    else:
-        # Add muscle forces on the rod
-        if os.path.exists("optimized_coefficients.txt"):
-            t_coeff_optimized = np.genfromtxt(
-                "optimized_coefficients.txt", delimiter=","
-            )
-        else:
-            wave_length = 1.0
-            t_coeff_optimized = np.array(
-                [3.4e-3, 3.3e-3, 4.2e-3, 2.6e-3, 3.6e-3, 3.5e-3]
-            )
-            t_coeff_optimized = np.hstack((t_coeff_optimized, wave_length))
-
-        # run the simulation
-        [avg_forward, avg_lateral, pp_list] = run_snake(
-            t_coeff_optimized, PLOT_FIGURE, SAVE_FIGURE, SAVE_VIDEO, SAVE_RESULTS
+        shearable_rod = CosseratRod.straight_rod(
+            n_elem,
+            start,
+            direction,
+            normal,
+            base_length,
+            base_radius,
+            density,
+            nu,
+            E,
+            shear_modulus=shear_modulus,
         )
 
-        print("average forward velocity:", avg_forward)
-        print("average forward lateral:", avg_lateral)
+        snake_sim.append(shearable_rod)
+
+        # Add gravitational forces
+        gravitational_acc = -9.80665
+        snake_sim.add_forcing_to(shearable_rod).using(
+            GravityForces, acc_gravity=np.array([0.0, gravitational_acc, 0.0])
+        )
+
+        # Add muscle torques
+        wave_length = b_coeff[-1]
+        snake_sim.add_forcing_to(shearable_rod).using(
+            MuscleTorques,
+            base_length=base_length,
+            b_coeff=b_coeff[:-1],
+            period=period,
+            wave_number=2.0 * np.pi / (wave_length),
+            phase_shift=0.0,
+            rest_lengths=shearable_rod.rest_lengths,
+            ramp_up_time=period,
+            direction=normal,
+            with_spline=True,
+        )
+
+        # Add friction forces
+        origin_plane = np.array([0.0, -base_radius, 0.0])
+        normal_plane = normal
+        slip_velocity_tol = 1e-8
+        froude = 0.1
+        mu = base_length / (period * period * np.abs(gravitational_acc) * froude)
+        kinetic_mu_array = np.array(
+            [mu, 1.5 * mu, 2.0 * mu]
+        )  # [forward, backward, sideways]
+        static_mu_array = np.zeros(kinetic_mu_array.shape)
+        snake_sim.add_forcing_to(shearable_rod).using(
+            AnisotropicFrictionalPlane,
+            k=1.0,
+            nu=1e-6,
+            plane_origin=origin_plane,
+            plane_normal=normal_plane,
+            slip_velocity_tol=slip_velocity_tol,
+            static_mu_array=static_mu_array,
+            kinetic_mu_array=kinetic_mu_array,
+        )
+
+        # Add call backs
+        class ContinuumSnakeCallBack(CallBackBaseClass):
+            """
+            Call back function for continuum snake
+            """
+
+            def __init__(self, step_skip: int, callback_params: dict):
+                CallBackBaseClass.__init__(self)
+                self.every = step_skip
+                self.callback_params = callback_params
+
+            def make_callback(self, system, time, current_step: int):
+
+                if current_step % self.every == 0:
+
+                    self.callback_params["time"].append(time)
+                    self.callback_params["step"].append(current_step)
+                    self.callback_params["position"].append(
+                        system.position_collection.copy()
+                    )
+                    self.callback_params["velocity"].append(
+                        system.velocity_collection.copy()
+                    )
+                    self.callback_params["avg_velocity"].append(
+                        system.compute_velocity_center_of_mass()
+                    )
+
+                    self.callback_params["center_of_mass"].append(
+                        system.compute_position_center_of_mass()
+                    )
+                    self.callback_params["curvature"].append(system.kappa.copy())
+
+                    return
+
+        pp_list = defaultdict(list)
+        snake_sim.collect_diagnostics(shearable_rod).using(
+            ContinuumSnakeCallBack, step_skip=step_skip, callback_params=pp_list
+        )
+
+        snake_sim.finalize()
+
+        timestepper = PositionVerlet()
+        integrate(timestepper, snake_sim, final_time, total_steps)

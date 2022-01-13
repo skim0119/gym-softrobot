d9f5f02f46ec78c350bc525095b8da9c5807b993
Seung Hyun Kim <skim449@illinois.edu>
2022-01-12 22:16:25 -0600

    disable incompleted environments

--- gym_softrobot/__init__.py
+++ gym_softrobot/__init__.py
@@ -6,29 +6,29 @@ register(
     entry_point='gym_softrobot.envs.octopus:FlatEnv',
 )
 
-register(
-    id='OctoReach-v0',
-    entry_point='gym_softrobot.envs.octopus:ReachEnv',
-)
+#register(
+#    id='OctoReach-v0',
+#    entry_point='gym_softrobot.envs.octopus:ReachEnv',
+#)
 
-register(
-    id='OctoSwim-v0',
-    entry_point='gym_softrobot.envs.octopus:SwimEnv',
-)
+#register(
+#    id='OctoSwim-v0',
+#    entry_point='gym_softrobot.envs.octopus:SwimEnv',
+#)
 
-register(
-    id='OctoHunt-v0',
-    entry_point='gym_softrobot.envs.octopus:HuntEnv',
-)
+#register(
+#    id='OctoHunt-v0',
+#    entry_point='gym_softrobot.envs.octopus:HuntEnv',
+#)
 
 """ Snake Environment """
-register(
-    id='ContinuumSnake-v0',
-    entry_point='gym_softrobot.envs.snake:ContinuumSnakeEnv',
-)
+#register(
+#    id='ContinuumSnake-v0',
+#    entry_point='gym_softrobot.envs.snake:ContinuumSnakeEnv',
+#)
 
 """ Simple Control Environment """
-register(
-    id='InertialPull-v0',
-    entry_point='gym_softrobot.envs.simple_control:InertialPullEnv',
-)
+#register(
+#    id='InertialPull-v0',
+#    entry_point='gym_softrobot.envs.simple_control:InertialPullEnv',
+#)

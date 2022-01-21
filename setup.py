0d20df94af587a28c0c92b68e6c3cc0c8dfcdda6
Seung Hyun Kim <skim449@illinois.edu>
2022-01-21 10:15:48 -0600

    bump up the version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = "https://github.com/skim0119/gym-softrobot"
 EMAIL = "skim449@illinois.edu"
 AUTHOR = "skim0119"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["gym>=0.21.0", "pyelastica>=0.2.0", "matplotlib>=3.3.2", "pyglet", "vapory"]
@@ -105,7 +105,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
     ],
-    download_url="https://github.com/skim0119/gym-softrobot/archive/refs/tags/v0.0.5.tar.gz",
+    download_url="https://github.com/skim0119/gym-softrobot/archive/refs/tags/v0.0.6.tar.gz",
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,

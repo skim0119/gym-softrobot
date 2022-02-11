bf1bbc77000e615e0b88fee78cf3ec3fe2dd2813
Seung Hyun Kim <skim449@illinois.edu>
2022-02-11 13:47:43 -0600

    update: arm push environment state space

--- gym_softrobot/envs/octopus/arm_push_env.py
+++ gym_softrobot/envs/octopus/arm_push_env.py
@@ -1,16 +1,13 @@
 from typing import Optional
 
 import gym
-from gym import core
-from gym import error, spaces, utils
+from gym import core, spaces
 from gym.utils import seeding
 
 from collections import defaultdict
 import time
-import copy
 
 import numpy as np
-from scipy.interpolate import interp1d
 
 from elastica import *
 from elastica.timestepper import extend_stepper_interface
@@ -21,7 +18,7 @@ from gym_softrobot.config import RendererType
 from gym_softrobot.utils.custom_elastica.callback_func import (
     RodCallBack,
 )
-from gym_softrobot.utils.render.post_processing import plot_video
+from gym_softrobot.utils.custom_elastica.constraint import ControllableFixConstraint
 from gym_softrobot.utils.render.base_renderer import (
     BaseRenderer,
     BaseElasticaRendererSession,
@@ -33,9 +30,7 @@ from gym_softrobot.utils.actuation.actuations.muscles.longitudinal_muscle import
 from gym_softrobot.utils.actuation.actuations.muscles.transverse_muscle import (
     TransverseMuscle,
 )
-from gym_softrobot.utils.actuation.actuations.muscles.apply_muscles import ApplyMuscle
-
-from gym_softrobot.utils.custom_elastica.constraint import ControllableFixConstraint
+from gym_softrobot.utils.actuation.actuations.muscles.muscle import ApplyMuscle
 
 
 class BaseSimulator(BaseSystemCollection, Constraints, Connections, Forcing, CallBacks):
@@ -60,9 +55,9 @@ class ArmPushEnv(core.Env):
         self,
         final_time: float = 2.5,
         time_step: float = 7.0e-5,
-        recording_fps: int = 5,
-        n_action: int = 3,
+        recording_fps: int = 40,
         config_generate_video: bool = False,
+        config_early_termination: bool = False,
     ):
 
         # Integrator type
@@ -74,11 +69,11 @@ class ArmPushEnv(core.Env):
         self.step_skip = int(1.0 / (recording_fps * time_step))
 
         # Spaces
-        self.action_space = spaces.Box(
-            action_low, action_high, shape=(n_action), dtype=np.float32
-        )
+        self.n_elem = 40
+        n_action = self.n_elem - 1
+        self.action_space = spaces.Box(0.0, 1.0, shape=(n_action), dtype=np.float32)
         self._observation_size = (
-            (self.n_seg + (self.n_elems + 1) * 4 + n_action + 2),
+            (self.n_elems + 1 + n_action),
         )  # 2 for target
         self.observation_space = spaces.Box(
             -np.inf, np.inf, shape=self._observation_size, dtype=np.float32
@@ -92,6 +87,7 @@ class ArmPushEnv(core.Env):
 
         # Configurations
         self.config_generate_video = config_generate_video
+        self.config_early_termination = config_early_termination
 
         # Rendering-related
         self.viewer = None
@@ -116,7 +112,7 @@ class ArmPushEnv(core.Env):
             self.simulator.collect_diagnostics(self.shearable_rod).using(
                 RodCallBack,
                 step_skip=self.step_skip,
-                callback_params=rod_parameters_dict,
+                callback_params=self.rod_parameters_dict,
             )
 
         """ Finalize the simulator and create time stepper """
@@ -127,7 +123,6 @@ class ArmPushEnv(core.Env):
         )
 
         self.time = np.float64(0.0)
-        self.counter = 0
 
         # Set Target
         self._target = (2 - 0.5) * self.np_random.random(2) + 0.5
@@ -137,18 +132,11 @@ class ArmPushEnv(core.Env):
         # Initial State
         state = self.get_state()
 
-        # Preprocessing
-        self.prev_dist_to_target = np.linalg.norm(
-            self.shearable_rod.compute_position_center_of_mass()[:2] - self._target,
-            ord=2,
-        )
-        # self.prev_cm_vel = self.shearable_rod.compute_velocity_center_of_mass()
-
         return state
 
     def _build(self):
         """Set up arm params"""
-        n_elem = 100
+        n_elem = self.n_elem
         L0 = 0.2
         radius_base = 0.012  # radius of the arm at the base
         radius_tip = 0.001  # radius of the arm at the tip
@@ -224,44 +212,29 @@ class ArmPushEnv(core.Env):
     def get_state(self):
         # Build state
         rod = self.shearable_rod
-        kappa_state = rod.kappa[0]
         pos_state1 = rod.position_collection[0]  # x
-        pos_state2 = rod.position_collection[1]  # y
-        vel_state1 = rod.velocity_collection[0]  # x
-        vel_state2 = rod.velocity_collection[1]  # y
         previous_action = self._prev_action.copy()
-        target = self._target
         state = np.hstack(
             [
-                kappa_state,
                 pos_state1,
-                pos_state2,
-                vel_state1,
-                vel_state2,
                 previous_action,
-                target,
             ]
         ).astype(np.float32)
         return state
 
     def set_action(self, action) -> None:
-        self._prev_action[:] = action
-        action = np.concatenate([[0], action, [0]], axis=-1)
-        action = interp1d(
-            np.linspace(0, 1, self.n_action + 2),  # added zero on the boundary
-            action,
-            kind="cubic",
-            axis=-1,
-        )(np.linspace(0, 1, self.n_seg))
-        self.shearable_rod.rest_kappa[0, :] = action  # Planar curvature
+        scale = 1.0  # min(time / 0.02, 1)
+        for muscle_count, muscle_layer in enumerate(self.muscle_layers):
+            muscle_layer.set_activation(action[muscle_count] * scale)
 
     def step(self, action):
         rest_kappa = action  # alias
 
         """ Set intrinsic strains (set actions) """
-        self.set_action(rest_kappa)
+        self.set_action(action)
 
         """ Post-simulation """
+        prev_cm_pos = self.shearable_rod.compute_position_center_of_mass()[:2]
 
         """ Run the simulation for one step """
         stime = time.perf_counter()
@@ -274,7 +247,6 @@ class ArmPushEnv(core.Env):
                 self.time_step,
             )
         etime = time.perf_counter()
-        # print(f'{self.counter=}, {etime-stime}sec, {self.time=}')
 
         """ Done is a boolean to reset the environment before episode is completed """
         done = False
@@ -291,23 +263,24 @@ class ArmPushEnv(core.Env):
             )
         )
 
-        if invalid_values_condition:
+        if self.config_early_termination:
+            done = self.check_early_termination()
+            survive_reward = -10.0
+        elif invalid_values_condition:
             print(f" Nan detected in, exiting simulation now. {self.time=}")
             done = True
             survive_reward = -50.0
         else:
             cm_pos = self.shearable_rod.compute_position_center_of_mass()[:2]
-            dist_to_target = np.linalg.norm(cm_pos - self._target, ord=2)
-            forward_reward = (self.prev_dist_to_target - dist_to_target) * 10
-            self.prev_dist_to_target = dist_to_target
-            """ Goal """
-            if dist_to_target < 0.1:
-                survive_reward = 100.0
-                done = True
+            moved_distance = np.linalg.norm(cm_pos, ord=2) - np.linalg.norm(
+                prev_cm_pos, ord=2
+            )
+            forward_reward = moved_distance * 10
 
         """ Time limit """
         timelimit = False
         if self.time > self.final_time:
+            survive_reward = np.linalg.norm(cm_pos, ord=2) * 10
             timelimit = True
             done = True
 
@@ -330,48 +303,7 @@ class ArmPushEnv(core.Env):
             "TimeLimit.truncated": timelimit,
         }
 
-        self.counter += 1
-
         return states, reward, done, info
-
-    def step(self, time, muscle_activations):
-
-        # set muscle activations
-        # self.muscle_layers[0].set_activation(muscle_activations[0])
-        scale = 1.0  # min(time / 0.02, 1)
-        for muscle_count in range(len(self.muscle_layers)):
-            self.muscle_layers[muscle_count].set_activation(
-                muscle_activations[muscle_count] * scale
-            )
-
-        # run the simulation for one step
-        time = self.do_step(
-            self.StatefulStepper,
-            self.stages_and_updates,
-            self.simulator,
-            time,
-            self.time_step,
-        )
-
-        if self.early_termination:
-            done = self.check_early_termination()
-        else:
-            """Done is a boolean to reset the environment before episode is completed"""
-            done = False
-            # Position of the rod cannot be NaN, it is not valid, stop the simulation
-            invalid_values_condition = _isnan_check(
-                self.shearable_rod.position_collection
-            )
-
-            if invalid_values_condition == True:
-                print(" Nan detected, exiting simulation now")
-                done = True
-            """ Done is a boolean to reset the environment before episode is completed """
-
-        systems = [self.shearable_rod]
-
-        return time, systems, done
-
 
     def render(self, mode="human", close=False):
         maxwidth = 800

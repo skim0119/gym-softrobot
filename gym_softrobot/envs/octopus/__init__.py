4c0646174a718b0b07e7a67dd3f1e7e2d4651732
Catuntu <chshih2@illinois.edu>
2022-03-24 15:35:26 -0500

    add two-arm-crawling env

--- gym_softrobot/envs/octopus/__init__.py
+++ gym_softrobot/envs/octopus/__init__.py
@@ -1,8 +1,10 @@
 from gym_softrobot.envs.octopus.flat_env import FlatEnv
 from gym_softrobot.envs.octopus.arm_single_env import ArmSingleEnv
+from gym_softrobot.envs.octopus.arm_two_env import ArmTwoEnv
 from gym_softrobot.envs.octopus.arm_push_env import ArmPushEnv
 from gym_softrobot.envs.octopus.arm_push_env import ArmPullWeightEnv
 from gym_softrobot.envs.octopus.reach_env import ReachEnv
 from gym_softrobot.envs.octopus.swim_env import SwimEnv
 from gym_softrobot.envs.octopus.crawl_env import CrawlEnv
+# from gym_softrobot.envs.octopus.crawlCurvature_env import CrawlCurvatureEnv
 from gym_softrobot.envs.octopus.hunt_env import HuntEnv

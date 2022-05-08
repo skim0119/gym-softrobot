041cac6f6535c51b4d84128e5bd52b48d00c0970
Seung Hyun Kim <skim449@illinois.edu>
2022-05-07 21:21:58 -0500

    fmt update

--- gym_softrobot/envs/octopus/build.py
+++ gym_softrobot/envs/octopus/build.py
@@ -1,4 +1,4 @@
-__doc__="""
+__doc__ = """
 Module contains elastica interface to create octopus model.
 
 We tried to select the mechanical parameters within the plausible range in soft-matter studies
@@ -29,33 +29,31 @@ from gym_softrobot.utils.actuation.actuations.muscles.muscle import ApplyMuscle
 
 from scipy.spatial.transform import Rotation as Rot
 
-_OCTOPUS_PROPERTIES = { # default parameters
-        # Arm properties
-        "youngs_modulus": 1e6,
-        "density": 1000.0,
-        "nu": 1e-2,
-        "poisson_ratio": 0.5,
-        # Head properties
-        "body_arm_k": 1e6,
-        "body_arm_kt": 1e0,
-        "head_radius": 0.04,
-        "head_density": 700.0,
-        # Friction Properties
-        "friction_multiplier": 1.00,
-        "friction_symmetry": False
-    }
+_OCTOPUS_PROPERTIES = {  # default parameters
+    # Arm properties
+    "youngs_modulus": 1e6,
+    "density": 1000.0,
+    "nu": 1e-2,
+    "poisson_ratio": 0.5,
+    # Head properties
+    "body_arm_k": 1e6,
+    "body_arm_kt": 1e0,
+    "head_radius": 0.04,
+    "head_density": 700.0,
+    # Friction Properties
+    "friction_multiplier": 1.00,
+    "friction_symmetry": False,
+}
 _DEFAULT_SCALE_LENGTH = {
-        "base_length": 0.35,
-        "base_radius": 0.35 * 0.02,
-    }
+    "base_length": 0.35,
+    "base_radius": 0.35 * 0.02,
+}
+
 
 def build_octopus(
-        simulator,
-        n_arm:int=8,
-        n_elem:int=11,
-        override_params:Optional[dict]=None
-    ):
-    """ Import default parameters (overridable) """
+    simulator, n_arm: int = 8, n_elem: int = 11, override_params: Optional[dict] = None
+):
+    """Import default parameters (overridable)"""
     param = _OCTOPUS_PROPERTIES.copy()  # Always copy parameter for safety
     if isinstance(override_params, dict):
         param.update(override_params)
@@ -63,19 +61,19 @@ def build_octopus(
     arm_scale_param = _DEFAULT_SCALE_LENGTH.copy()
 
     """ Set up an arm """
-    L0 = arm_scale_param['base_length']
-    r0 = arm_scale_param['base_radius']
+    L0 = arm_scale_param["base_length"]
+    r0 = arm_scale_param["base_radius"]
 
     rigid_rod_length = r0 * 2
-    rigid_rod_radius = param['head_radius']
+    rigid_rod_radius = param["head_radius"]
 
-    rotation_angle=360/n_arm
-    angle_list=[rotation_angle*arm_i for arm_i in range(n_arm)]
+    rotation_angle = 360 / n_arm
+    angle_list = [rotation_angle * arm_i for arm_i in range(n_arm)]
 
-    shearable_rods=[]  # arms
+    shearable_rods = []  # arms
     for arm_i in range(n_arm):
         arm_angle = angle_list[arm_i]
-        rot = Rot.from_euler('z', arm_angle, degrees=True)
+        rot = Rot.from_euler("z", arm_angle, degrees=True)
         arm_pos = rot.apply([rigid_rod_radius, 0.0, 0.0])
         arm_dir = rot.apply([1.0, 0.0, 0.0])
         rod = CosseratRod.straight_rod(
@@ -91,42 +89,57 @@ def build_octopus(
         simulator.append(rod)
 
     """ Add head """
-    start = np.zeros((3,)); start[2] = -r0
+    start = np.zeros((3,))
+    start[2] = -r0
     direction = np.array([0.0, 0.0, 1.0])
     normal = np.array([0.0, 1.0, 0.0])
     binormal = np.cross(direction, normal)
     base_area = np.pi * rigid_rod_radius ** 2
-    density = param['head_density']
+    density = param["head_density"]
 
-    rigid_rod = Cylinder(start, direction, normal, rigid_rod_length, rigid_rod_radius, density)
+    rigid_rod = Cylinder(
+        start, direction, normal, rigid_rod_length, rigid_rod_radius, density
+    )
     simulator.append(rigid_rod)
 
     """ Constraint body """
     simulator.constrain(rigid_rod).using(
         # Upright rigid rod need restoration force/torque against the floor
-        BodyBoundaryCondition, constrained_position_idx=(0,), constrained_director_idx=(0,)
+        BodyBoundaryCondition,
+        constrained_position_idx=(0,),
+        constrained_director_idx=(0,),
     )
 
     """ Set up boundary conditions """
     for arm_i in range(n_arm):
-        _k = param['body_arm_k']
-        _kt = param['body_arm_kt']
+        _k = param["body_arm_k"]
+        _kt = param["body_arm_kt"]
         simulator.connect(
-            first_rod=rigid_rod, second_rod=shearable_rods[arm_i], first_connect_idx=-1, second_connect_idx=0
-        ).using(FixedJoint2Rigid, k=_k, nu=1e-3, kt=_kt,angle=angle_list[arm_i],radius=rigid_rod_radius)
+            first_rod=rigid_rod,
+            second_rod=shearable_rods[arm_i],
+            first_connect_idx=-1,
+            second_connect_idx=0,
+        ).using(
+            FixedJoint2Rigid,
+            k=_k,
+            nu=1e-3,
+            kt=_kt,
+            angle=angle_list[arm_i],
+            radius=rigid_rod_radius,
+        )
 
     """Add gravity forces"""
     _g = -9.81
     gravitational_acc = np.array([0.0, 0.0, _g])
     for arm_i in range(n_arm):
         simulator.add_forcing_to(shearable_rods[arm_i]).using(
-                GravityForces, acc_gravity=gravitational_acc
-            )
-    '''
+            GravityForces, acc_gravity=gravitational_acc
+        )
+    """
     simulator.add_forcing_to(rigid_rod).using(
                 GravityForces, acc_gravity=gravitational_acc
             )
-    '''
+    """
 
     """ Add drag force """
     # dl = L0 / n_elem
@@ -155,14 +168,14 @@ def build_octopus(
     slip_velocity_tol = 1e-8
     froude = 0.1
     mu = L0 / (period * period * np.abs(_g) * froude)
-    if param['friction_symmetry']:
-        kinetic_mu_array = np.array(
-            [mu, mu, mu]
-        ) * param['friction_multiplier'] # [forward, backward, sideways]
+    if param["friction_symmetry"]:
+        kinetic_mu_array = (
+            np.array([mu, mu, mu]) * param["friction_multiplier"]
+        )  # [forward, backward, sideways]
     else:
-        kinetic_mu_array = np.array(
-            [mu, 1.5 * mu, 2.0 * mu]
-        ) * param['friction_multiplier'] # [forward, backward, sideways]
+        kinetic_mu_array = (
+            np.array([mu, 1.5 * mu, 2.0 * mu]) * param["friction_multiplier"]
+        )  # [forward, backward, sideways]
     static_mu_array = 2 * kinetic_mu_array
     for arm_i in range(n_arm):
         simulator.add_forcing_to(shearable_rods[arm_i]).using(
@@ -175,7 +188,7 @@ def build_octopus(
             static_mu_array=static_mu_array,
             kinetic_mu_array=kinetic_mu_array,
         )
-    '''
+    """
     mu = L0 / (period * period * np.abs(_g) * froude)
     kinetic_mu_array = np.array([mu, mu, mu])  # [forward, backward, sideways]
     static_mu_array = 2 * kinetic_mu_array
@@ -189,18 +202,19 @@ def build_octopus(
         static_mu_array=static_mu_array,
         kinetic_mu_array=kinetic_mu_array,
     )
-    '''
+    """
 
     return shearable_rods, rigid_rod
 
+
 def build_arm(
-        simulator,
-        n_elem:int=11,
-        override_params:Optional[dict]=None,
-        attach_head:bool=None, # TODO: To be implemented
-        attach_weight:Optional[bool]=None, # TODO: To be implemented
-    ):
-    """ Import default parameters (overridable) """
+    simulator,
+    n_elem: int = 11,
+    override_params: Optional[dict] = None,
+    attach_head: bool = None,  # TODO: To be implemented
+    attach_weight: Optional[bool] = None,  # TODO: To be implemented
+):
+    """Import default parameters (overridable)"""
     param = _OCTOPUS_PROPERTIES.copy()  # Always copy parameter for safety
     if isinstance(override_params, dict):
         param.update(override_params)
@@ -208,8 +222,8 @@ def build_arm(
     arm_scale_param = _DEFAULT_SCALE_LENGTH.copy()
 
     """ Set up an arm """
-    L0 = arm_scale_param['base_length']
-    r0 = arm_scale_param['base_radius']
+    L0 = arm_scale_param["base_length"]
+    r0 = arm_scale_param["base_radius"]
 
     arm_pos = np.array([0.0, 0.0, 0.0])
     arm_dir = np.array([1.0, 0.0, 0.0])
@@ -227,26 +241,24 @@ def build_arm(
     """Add gravity forces"""
     _g = -9.81
     gravitational_acc = np.array([0.0, 0.0, _g])
-    simulator.add_forcing_to(rod).using(
-            GravityForces, acc_gravity=gravitational_acc
-        )
+    simulator.add_forcing_to(rod).using(GravityForces, acc_gravity=gravitational_acc)
 
     """Add friction forces (always the last thing before finalize)"""
-    contact_k = 1e2 # TODO: These need to be global parameter to tune
+    contact_k = 1e2  # TODO: These need to be global parameter to tune
     contact_nu = 1e1
     period = 2.0
     origin_plane = np.array([0.0, 0.0, -r0])
     slip_velocity_tol = 1e-8
     froude = 0.1
     mu = L0 / (period * period * np.abs(_g) * froude)
-    if param['friction_symmetry']:
-        kinetic_mu_array = np.array(
-            [mu, mu, mu]
-        ) * param['friction_multiplier'] # [forward, backward, sideways]
+    if param["friction_symmetry"]:
+        kinetic_mu_array = (
+            np.array([mu, mu, mu]) * param["friction_multiplier"]
+        )  # [forward, backward, sideways]
     else:
-        kinetic_mu_array = np.array(
-            [mu, 1.5 * mu, 2.0 * mu]
-        ) * param['friction_multiplier'] # [forward, backward, sideways]
+        kinetic_mu_array = (
+            np.array([mu, 1.5 * mu, 2.0 * mu]) * param["friction_multiplier"]
+        )  # [forward, backward, sideways]
     static_mu_array = 2 * kinetic_mu_array
     simulator.add_forcing_to(rod).using(
         AnisotropicFrictionalPlane,
@@ -261,10 +273,11 @@ def build_arm(
 
     return rod
 
+
 def create_es_muscle_layers(
-        radius_mean,
-        radius_base,
-    ):
+    radius_mean,
+    radius_base,
+):
     muscle_layers = [
         # LongitudinalMuscle(
         #     muscle_radius_ratio=np.stack(
--- gym_softrobot/envs/octopus/build_muscle_octopus.py
+++ gym_softrobot/envs/octopus/build_muscle_octopus.py
@@ -1,4 +1,4 @@
-__doc__="""
+__doc__ = """
 Module contains elastica interface to create octopus model.
 This model specifically create 8-arm octopus with each arm controlled by muscle model.
 Suposedly, this model represend simplified control of arm coordination.
@@ -17,7 +17,8 @@ from elastica.experimental.interaction import AnisotropicFrictionalPlaneRigidBod
 
 from gym_softrobot.utils.custom_elastica.joint import FixedJoint2Rigid
 from gym_softrobot.utils.custom_elastica.constraint import BodyBoundaryCondition
-#from gym_softrobot.utils.actuation.forces.drag_force import DragForce
+
+# from gym_softrobot.utils.actuation.forces.drag_force import DragForce
 from gym_softrobot.utils.actuation.actuations.muscles.longitudinal_muscle import (
     LongitudinalMuscle,
 )
@@ -35,7 +36,7 @@ ARM_MATERIAL = {
     # Arm properties
     "density": 1000.0,
     "youngs_modulus": 1.5e4,
-    "shear_modulus": 1.5e4 / (1.0+0.5), # 0.5 Poisson Ratio
+    "shear_modulus": 1.5e4 / (1.0 + 0.5),  # 0.5 Poisson Ratio
     "nu": 0.20,
     "nu_scale": 1e-2,
 }
@@ -43,7 +44,7 @@ DEFAULT_SCALE_LENGTH = {
     # Arm length scale
     "base_length": 0.25,
     "base_radius": 0.013,
-    "tip_radius" : 0.0042, # https://www.liebertpub.com/doi/10.1089/soro.2019.0082
+    "tip_radius": 0.0042,  # https://www.liebertpub.com/doi/10.1089/soro.2019.0082
     "head_radius": 0.04,
 }
 HEAD_PROPERTIES = {
@@ -55,8 +56,11 @@ HEAD_PROPERTIES = {
     "body_arm_nu": 1e-3,
 }
 
-def build_arm(n_elem:int, start:np.ndarray, direction:np.ndarray, normal:np.ndarray) -> CosseratRod:
-    """ Set up properties """
+
+def build_arm(
+    n_elem: int, start: np.ndarray, direction: np.ndarray, normal: np.ndarray
+) -> CosseratRod:
+    """Set up properties"""
     arm_material = deepcopy(ARM_MATERIAL)
     arm_material["n_elements"] = n_elem
     arm_material["start"] = start
@@ -64,13 +68,15 @@ def build_arm(n_elem:int, start:np.ndarray, direction:np.ndarray, normal:np.ndar
     arm_material["normal"] = normal
     arm_material["base_length"] = DEFAULT_SCALE_LENGTH["base_length"]
     arm_material["base_radius"] = np.linspace(
-            DEFAULT_SCALE_LENGTH["base_radius"], 
-            DEFAULT_SCALE_LENGTH["tip_radius"],
-            n_elem)
-    arm_material["nu"] *= ((arm_material["base_radius"]/DEFAULT_SCALE_LENGTH["base_radius"])**2.0)*arm_material["nu_scale"]
+        DEFAULT_SCALE_LENGTH["base_radius"], DEFAULT_SCALE_LENGTH["tip_radius"], n_elem
+    )
+    arm_material["nu"] *= (
+        (arm_material["base_radius"] / DEFAULT_SCALE_LENGTH["base_radius"]) ** 2.0
+    ) * arm_material["nu_scale"]
 
     rod = CosseratRod.straight_rod(**arm_material)
     return rod
+
 
 def build_octopus_muscles(simulator, n_elem: int = 40):
     """
@@ -86,9 +92,9 @@ def build_octopus_muscles(simulator, n_elem: int = 40):
     n_arm = 8
 
     """ Set up an arm """
-    L0 = DEFAULT_SCALE_LENGTH['base_length']
-    r0 = DEFAULT_SCALE_LENGTH['base_radius']
-    head_radius = DEFAULT_SCALE_LENGTH['head_radius']
+    L0 = DEFAULT_SCALE_LENGTH["base_length"]
+    r0 = DEFAULT_SCALE_LENGTH["base_radius"]
+    head_radius = DEFAULT_SCALE_LENGTH["head_radius"]
 
     # Levi (2015) - Figure 3B
     shearable_rods = []  # arms
@@ -96,7 +102,7 @@ def build_octopus_muscles(simulator, n_elem: int = 40):
     angles = [angles_offset + 45 * arm_i for arm_i in range(n_arm)]
     for angle in angles:
         # Rotate director
-        rot = Rot.from_euler('z', angle, degrees=True)
+        rot = Rot.from_euler("z", angle, degrees=True)
         arm_pos = rot.apply([head_radius, 0.0, 0.0])
         arm_dir = rot.apply([1.0, 0.0, 0.0])
         arm_normal = np.array([0.0, 0.0, 1.0])
@@ -106,36 +112,39 @@ def build_octopus_muscles(simulator, n_elem: int = 40):
         simulator.append(rod)
 
     """ Add head """
-    start = np.zeros((3,)); start[2] = -r0 * 2
+    start = np.zeros((3,))
+    start[2] = -r0 * 2
     direction = np.array([0.0, 0.0, 1.0])
     normal = np.array([0.0, 1.0, 0.0])
-    density = HEAD_PROPERTIES['head_density']
+    density = HEAD_PROPERTIES["head_density"]
     rigid_rod = Cylinder(start, direction, normal, r0 * 2, head_radius, density)
     simulator.append(rigid_rod)
 
     """ Constraint body """
     simulator.constrain(rigid_rod).using(
         # Upright rigid rod need restoration force/torque against the floor
-        BodyBoundaryCondition, constrained_position_idx=(0,), constrained_director_idx=(0,)
+        BodyBoundaryCondition,
+        constrained_position_idx=(0,),
+        constrained_director_idx=(0,),
     )
 
     """ Set up boundary conditions """
-    _k = HEAD_PROPERTIES['body_arm_k']
-    _kt = HEAD_PROPERTIES['body_arm_kt']
-    _nu = HEAD_PROPERTIES['body_arm_nu']
+    _k = HEAD_PROPERTIES["body_arm_k"]
+    _kt = HEAD_PROPERTIES["body_arm_kt"]
+    _nu = HEAD_PROPERTIES["body_arm_nu"]
     for arm_i in range(n_arm):
         simulator.connect(
             first_rod=rigid_rod,
             second_rod=shearable_rods[arm_i],
             first_connect_idx=-1,
-            second_connect_idx=0
+            second_connect_idx=0,
         ).using(
             FixedJoint2Rigid,
             k=_k,
             nu=_nu,
             kt=_kt,
             angle=angles[arm_i],
-            radius=head_radius
+            radius=head_radius,
         )
 
     """ Add drag force """
@@ -162,8 +171,7 @@ def build_octopus_muscles(simulator, n_elem: int = 40):
     tm_activations = []
     for i in range(n_arm):
         muscle_layers = create_es_muscle_layers(
-            shearable_rods[i].radius,
-            DEFAULT_SCALE_LENGTH["base_radius"]
+            shearable_rods[i].radius, DEFAULT_SCALE_LENGTH["base_radius"]
         )
         simulator.add_forcing_to(shearable_rods[i]).using(
             ApplyMuscle,
@@ -174,6 +182,7 @@ def build_octopus_muscles(simulator, n_elem: int = 40):
         tm_activations.append(muscle_layers)  # 2 for TM
 
     return shearable_rods, rigid_rod, tm_activations
+
 
 def build_two_arms(simulator, n_elem: int = 40):
     """
@@ -189,9 +198,9 @@ def build_two_arms(simulator, n_elem: int = 40):
     n_arm = 2
 
     """ Set up an arm """
-    L0 = DEFAULT_SCALE_LENGTH['base_length']
-    r0 = DEFAULT_SCALE_LENGTH['base_radius']
-    head_radius = DEFAULT_SCALE_LENGTH['head_radius']
+    L0 = DEFAULT_SCALE_LENGTH["base_length"]
+    r0 = DEFAULT_SCALE_LENGTH["base_radius"]
+    head_radius = DEFAULT_SCALE_LENGTH["head_radius"]
 
     # Levi (2015) - Figure 3B
     shearable_rods = []  # arms
@@ -199,7 +208,7 @@ def build_two_arms(simulator, n_elem: int = 40):
     angles = [angles_offset + 180.0 * arm_i for arm_i in range(n_arm)]
     for angle in angles:
         # Rotate director
-        rot = Rot.from_euler('z', angle, degrees=True)
+        rot = Rot.from_euler("z", angle, degrees=True)
         arm_pos = rot.apply([head_radius, 0.0, 0.0])
         arm_dir = rot.apply([1.0, 0.0, 0.0])
         arm_normal = np.array([0.0, 0.0, 1.0])
@@ -209,37 +218,39 @@ def build_two_arms(simulator, n_elem: int = 40):
         simulator.append(rod)
 
     """ Add head """
-    start = np.zeros((3,));
+    start = np.zeros((3,))
     start[2] = -r0 * 2
     direction = np.array([0.0, 0.0, 1.0])
     normal = np.array([0.0, 1.0, 0.0])
-    density = HEAD_PROPERTIES['head_density']
+    density = HEAD_PROPERTIES["head_density"]
     rigid_rod = Cylinder(start, direction, normal, r0 * 2, head_radius, density)
     simulator.append(rigid_rod)
 
     """ Constraint body """
     simulator.constrain(rigid_rod).using(
         # Upright rigid rod need restoration force/torque against the floor
-        BodyBoundaryCondition, constrained_position_idx=(0,), constrained_director_idx=(0,)
+        BodyBoundaryCondition,
+        constrained_position_idx=(0,),
+        constrained_director_idx=(0,),
     )
 
     """ Set up boundary conditions """
-    _k = HEAD_PROPERTIES['body_arm_k']
-    _kt = HEAD_PROPERTIES['body_arm_kt']
-    _nu = HEAD_PROPERTIES['body_arm_nu']
+    _k = HEAD_PROPERTIES["body_arm_k"]
+    _kt = HEAD_PROPERTIES["body_arm_kt"]
+    _nu = HEAD_PROPERTIES["body_arm_nu"]
     for arm_i in range(n_arm):
         simulator.connect(
             first_rod=rigid_rod,
             second_rod=shearable_rods[arm_i],
             first_connect_idx=-1,
-            second_connect_idx=0
+            second_connect_idx=0,
         ).using(
             FixedJoint2Rigid,
             k=_k,
             nu=_nu,
             kt=_kt,
             angle=angles[arm_i],
-            radius=head_radius
+            radius=head_radius,
         )
 
     """ Add drag force """
@@ -266,8 +277,7 @@ def build_two_arms(simulator, n_elem: int = 40):
     arm_activations = []
     for i in range(n_arm):
         muscle_layers = create_es_muscle_layers(
-            shearable_rods[i].radius,
-            DEFAULT_SCALE_LENGTH["base_radius"]
+            shearable_rods[i].radius, DEFAULT_SCALE_LENGTH["base_radius"]
         )
         simulator.add_forcing_to(shearable_rods[i]).using(
             ApplyMuscle,

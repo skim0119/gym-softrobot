6c937c62c716a854232e77754a1bda5f11484d4c
Seung Hyun Kim <skim0119@gmail.com>
2026-07-27 17:10:44 -0500

    Remove legacy environment and modernize tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,5 +1,3 @@
-import pytest
-
 """ Test Dependency Installation
 
 The purpose is to check if core dependencies are installed properly.
@@ -7,10 +5,12 @@ Typically, failure to these tests indicate an incorrection installation
 or wrong activation of the virtual environment (i.e. conda, venv, etc.).
 
 """
-def test_gym():
-    import gym
-    version = gym.version.VERSION
-    assert version
+def test_gymnasium():
+    import gymnasium
+
+    assert gymnasium.__version__
 
 def test_pyelastica():
-    import elastica
+    from importlib.metadata import version
+
+    assert version("pyelastica")
